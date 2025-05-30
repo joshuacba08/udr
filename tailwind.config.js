f5ed0462f604d633562b47f4b5e236fa68494957
Josue <joshuacba08@gmail.com>
2025-05-29 23:04:18 -0300

    Add @material-tailwind/react dependency and update styles

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+import { mtConfig } from "@material-tailwind/react";
+
+/** @type {import('tailwindcss').Config} */
+
+export default {
+  content: [
+    "./index.html",
+
+    "./src/**/*.{js,ts,jsx,tsx}",
+
+    "./node_modules/@material-tailwind/react/**/*.{js,ts,jsx,tsx}",
+  ],
+
+  theme: {
+    extend: {
+      colors: {
+        primary: "#3279bd",
+        secondary: "#ffffff",
+        error: "#e91248",
+        success: "#35b890",
+        warning: "#ffb503",
+      },
+      fontFamily: {
+        "gt-walsheim": ["GT Walsheim", "sans-serif"],
+      },
+    },
+  },
+
+  plugins: [mtConfig],
+};
